bdd519899b03ad0ee271f71c21ea005852102521
triledev <tri.le.mail@gmail.com>
2025-11-02 12:16:06 -0600

    Initial Commit

---
+++ AllTrailsLunchApp/AllTrailsLunchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllTrailsLunchAppApp.swift
+//  AllTrailsLunchApp
+//
+//  Created by Tri Le on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AllTrailsLunchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllTrailsLunchAppTests/AllTrailsLunchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllTrailsLunchAppTests.swift
+//  AllTrailsLunchAppTests
+//
+//  Created by Tri Le on 11/2/25.
+//
+
+import Testing
+@testable import AllTrailsLunchApp
+
+struct AllTrailsLunchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
